107394693f7205fe031abd15d9818a240fef3b1e
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-16 12:10:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
   delete object[key];
   return object;
 }
